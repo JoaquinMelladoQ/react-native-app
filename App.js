8e8a321affe26e968f076e1e847f407c240cc7c7
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2021-03-10 13:18:14 -0300

    Adding stack routes

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
-export default function App() {
+function UsersScreen() {
   return (
     <View style={styles.container}>
       <Text>APP!!!!</Text>
@@ -10,6 +12,35 @@ export default function App() {
     </View>
   );
 }
+function PostsScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>APP!!!!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+function DetailScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>APP!!!!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+const AppNavigator = createStackNavigator({
+  Users: {
+    screen: UsersScreen,
+  },
+  Posts: {
+    screen: PostsScreen,
+  },
+  Detail: {
+    screen: DetailScreen,
+  }
+})
+
+export default createAppContainer(AppNavigator)
 
 const styles = StyleSheet.create({
   container: {
