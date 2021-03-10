35162cac1d3b5aed6347b5319a021d1f2577497a
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2021-03-10 17:25:26 -0300

    Rendering title from posts api, destructuring navigation to reach data correctly

--- screens/Posts.js
+++ screens/Posts.js
@@ -1,21 +1,47 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Text, FlatList } from 'react-native';
+import ListItem from '../components/ListItem';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
   },
+  list: {
+    alignSelf: 'stretch'
+  }
 });
 
-export default () => {
+export default ({ navigation }) => {
+  const userId = navigation.getParam('user_id') 
+  const [loading, setLoading] = useState(true)
+  const [posts, setPosts] = useState([])
+
+  const fetchPosts = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const data = await response.json()
+    setPosts(data)
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    fetchPosts()
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Text>APP!!!!</Text>
-      <StatusBar style="auto" />
-    </View>
+  <View style={styles.container}>
+      {loading ? <Text>Cargando...</Text> : 
+      <FlatList
+        style={styles.list}
+        data={posts.filter(x => x.userId === userId)}
+        keyExtractor={x => String(x.id)}
+        renderItem={({ item }) => <ListItem 
+          onPress={() => navigation.navigate('Posts', { user_id: item.id })} 
+          title={item.title}
+        />}
+      />}
+  </View>
   );
 }
