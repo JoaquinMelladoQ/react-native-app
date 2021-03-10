4a552a7bc6a4ade16137f27063161b9d3b790956
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2021-03-10 13:59:19 -0300

    Created dummy data and rendered with flatlist

--- screens/Users.js
+++ screens/Users.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, FlatList } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
   },
 });
+
+const users = [
+  { id: 1, name: 'Leanne'},
+  { id: 2, name: 'Ervin'},
+]
 
 export default () => {
   return (
     <View style={styles.container}>
-      <Text>APP!!!!</Text>
+      <FlatList
+        data={users}
+        keyExtractor={x => x.id}
+        renderItem={({ item }) => <Text>{item.name}</Text>}
+      />
       <StatusBar style="auto" />
     </View>
   );
